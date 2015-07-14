b5a468068bbc77a02a97d2763338dec58ebdfebf
Christopher Chang <chiggiechang@gmail.com>
2015-07-13 21:27:01 -0700

    Added package-install commands, and removed unnecessary packages.

--- .emacs.d/config/plugins.el
+++ .emacs.d/config/plugins.el
@@ -159,6 +159,7 @@
 (setq indent-guide-recursive t)
 
 ;; Paredit
+(package-install 'paredit)
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
@@ -166,6 +167,10 @@
 (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+;; Evil-Paredit
+(package-install 'evil-paredit)
+(add-hook 'emacs-lisp-mode-hook 'evil-paredit-mode)
 
 ;; CC mode
 (require 'cc-mode)
@@ -191,10 +196,6 @@
 (setq c-eldoc-includes "`pkg-config gtk+-2.0 --cflags` -I./ -I../ ")
 (load "c-eldoc")
 (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
-
-;; Semantic-refactor, for C and lisp
-(require 'srefactor)
-(require 'srefactor-lisp)
 
 ;; OPTIONAL: ADD IT ONLY IF YOU USE C/C++. 
 (semantic-mode 1) ;; -> this is optional for Lisp
