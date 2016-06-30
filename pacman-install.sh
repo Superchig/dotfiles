654ee7d523d39d0babf938d82daf374bdfe3aaf2
Christopher Chang <chiggiechang@gmail.com>
2016-06-29 18:32:00 -0700

    pacman-install.sh now installs Java (openjre, openjdk), conky, neofetch, and ghetto-skype

--- pacman-install.sh
+++ pacman-install.sh
@@ -5,6 +5,10 @@ IFS=$'\n\t'
 
 AUR_HELPER=pacaur
 
-sudo pacman -S zsh emacs guake chromium vlc mpv
+sudo pacman -S zsh emacs guake chromium vlc mpv gimp htop scrot aria2 \
+	 jre7-openjdk jdk7-openjdk jre8-openjdk jdk8-openjdk \
+	 conky
+
+$AUR_HELPER -S neofetch ghetto-skype
 
 ./distro-agnostic-scripts-install.sh
