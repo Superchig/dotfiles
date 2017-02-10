0e7cf618ad165cd141c839fe9512eea38c892951
Christopher Chang <chiggiechang@gmail.com>
2017-02-10 15:49:42 -0800

    Changed org-mode keybindings again.

--- .emacs.d/config/rebind.el
+++ .emacs.d/config/rebind.el
@@ -1,9 +1,9 @@
 ;;;; Bind keys in various modes
 (defun org-evil-mode-config ()
   "For use in `org-mode-hook'."
-  (local-set-key (kbd "M-l") 'org-shiftright)
-  (local-set-key (kbd "M-h") 'org-shiftleft)
-  (local-set-key (kbd "M-k") 'org-move-item-up)
-  (local-set-key (kbd "M-j") 'org-move-item-down))
+  (local-set-key (kbd "M-l") 'org-metaright)
+  (local-set-key (kbd "M-h") 'org-metaleft)
+  (local-set-key (kbd "M-k") 'org-metaup)
+  (local-set-key (kbd "M-j") 'org-metadown))
 
 (add-hook 'org-mode-hook 'org-evil-mode-config)
