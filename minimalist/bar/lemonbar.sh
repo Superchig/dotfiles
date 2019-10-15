ffd5f6ffcce32b6aa5cbcf18d4d142b7005f4237
Christopher Chang <chiggiechang@gmail.com>
2019-10-14 19:08:49 -0700

    Changed bar and .cwmrc

--- minimalist/bar/lemonbar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-# Kill all existing lemonbar instances
-killall -q lemonbar
-
-sh ./_lemonbar.sh | lemonbar -p -d -f Inconsolata &
--- minimalist/cwm-custom.sh
+++ minimalist/cwm-custom.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # Startup script for cwm, used mainly to autostart programs
 
-bash /home/chiggie/dotfiles/minimalist/poststartup &
+bash "$HOME"/dotfiles/minimalist/cwm-poststartup.bash &
 
 exec cwm
