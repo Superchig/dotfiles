724d7d8b0c5cfefa8a48abee0421d0672342d916
Christopher Chang <chiggiechang@gmail.com>
2020-02-03 17:09:52 -0800

    Remove additional function

--- util/widener.sh
+++ util/widener.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 # Requires xsel and rofi
 
-# Function from
-# https://stackoverflow.com/questions/1527049/how-can-i-join-elements-of-an-array-in-bash
-function join_by { local IFS="$1"; shift; echo "$*"; }
-
-join_by ' ' $(rofi -dmenu -p "insert text" | grep -o .) | tr -d '\n' | xsel -ib
+rofi -dmenu -p "insert text" | grep -o . | tr '\n' ' ' | sed 's/.$//' | xsel -ib
