c0bb1991d005235c0fc24373d003fca5e1327fd1
Christopher Chang <chiggiechang@gmail.com>
2020-03-05 10:25:37 -0800

    Fix bug with and add feature to monitor script
    Make it use ~/.fehbg if it exists

--- minimalist/monitor_setup.bash
+++ minimalist/monitor_setup.bash
@@ -1,12 +1,16 @@
 #!/usr/bin/bash
 
 # If there are no command-line arguments and the HDMI1 monitor is plugged in, activate HDMI1
-if [ "$#" -eq 0 ] && xrandr | grep HDMI1; then
+if [ "$#" -eq 0 ] && xrandr | grep 'HDMI1 connected'; then
   xrandr --output eDP1 --mode 1366x768
   xrandr --output HDMI1 --mode 1920x1080 --left-of eDP1 --primary
-elif [ "$1" = "nobuiltin" ] && xrandr | grep HDMI1; then
+elif [ "$1" = "nobuiltin" ] && xrandr | grep 'HDMI1 connected'; then
   xrandr --output eDP1 --off
   xrandr --output HDMI1 --mode 1920x1080
 else
   xrandr --auto
+fi
+
+if [ -f ~/.fehbg ]; then
+  ~/.fehbg
 fi
