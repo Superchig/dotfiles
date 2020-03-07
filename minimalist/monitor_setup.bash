360708c7cba58adceeb95016c9d56c06f65a1895
Christopher Chang <chiggiechang@gmail.com>
2020-03-07 00:00:57 -0800

    Made monitor setup script slightly more flexible

--- minimalist/monitor_setup.bash
+++ minimalist/monitor_setup.bash
@@ -1,16 +1,25 @@
 #!/usr/bin/bash
 
+# Select eDP1 or eDP2 as the built-in screen, depending on which one is
+# connected
+if xrandr | grep 'eDP1 connected'; then
+  eDP=eDP1
+else
+  eDP=eDP2
+fi
+
 # If there are no command-line arguments and the HDMI1 monitor is plugged in, activate HDMI1
 if [ "$#" -eq 0 ] && xrandr | grep 'HDMI1 connected'; then
-  xrandr --output eDP1 --mode 1366x768
-  xrandr --output HDMI1 --mode 1920x1080 --left-of eDP1 --primary
+  xrandr --output $eDP --mode 1366x768
+  xrandr --output HDMI1 --mode 1920x1080 --left-of $eDP --primary
 elif [ "$1" = "nobuiltin" ] && xrandr | grep 'HDMI1 connected'; then
-  xrandr --output eDP1 --off
+  xrandr --output $eDP --off
   xrandr --output HDMI1 --mode 1920x1080
 else
   xrandr --auto
 fi
 
+# Use ~/.fehbg if it exists
 if [ -f ~/.fehbg ]; then
   ~/.fehbg
 fi
