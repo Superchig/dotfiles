9764c0b4d8e5502323ebab35a3438b77a59a7523
Christopher Chang <chiggiechang@gmail.com>
2020-04-05 20:34:56 -0700

    Set up high dpi scaling

--- pacman-install.sh
+++ pacman-install.sh
@@ -9,7 +9,7 @@ sudo pacman --needed -S fish xorg-server xfce4 neovim xsel base-devel vim git \
 	firefox xorg-xrandr arandr sxiv guake gcc clang fd exa xterm \
 	i3-gaps rofi autorandr feh picom redshift alacritty mpv wget curl \
 	xss-lock vlc pulseaudio pulseaudio-alsa pavucontrol perl-file-mimeinfo \
-	discord shellcheck gnome-keyring seahorse \
+	discord shellcheck gnome-keyring seahorse qalculate-gtk \
 	adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
 
 # Install yay
