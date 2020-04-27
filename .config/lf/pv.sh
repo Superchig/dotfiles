32b160066f485bb5a544035778cfb7226361e296
Christopher Chang <chiggiechang@gmail.com>
2020-04-27 11:35:27 -0700

    Use mediainfo to get video file information in lf

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -9,5 +9,7 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
+    *.mp4) mediainfo "$1";;
+    *.mkv) mediainfo "$1";;
     *) highlight -O ansi "$1" || cat "$1";;
 esac
