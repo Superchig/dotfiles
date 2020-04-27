ec73bdb96acbd44a15809cd1189ad7daf6d8640b
Christopher Chang <chiggiechang@gmail.com>
2020-04-27 00:47:34 -0700

    Update fish prompt after using keybinding for lf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -71,8 +71,8 @@ set -x LESS_TERMCAP_so (printf '\e[01;33m')
 set -x LESS_TERMCAP_ue (printf '\e[0m')
 set -x LESS_TERMCAP_us (printf '\e[1;4;31m')
 
-# Bind f function (open lf with cd on quit enabled) to Ctrl+o
-bind \co f
+# Bind file manager to Ctrl+o
+bind \co 'f; commandline -f repaint'
 
 # Source the file for z.fish
 . $HOME/dotfiles/z.fish
