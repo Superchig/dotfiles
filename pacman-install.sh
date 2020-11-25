b2e9d32797cb04474753e3ed7c0a9055c39a878b
Christopher Chang <chiggiechang@gmail.com>
2020-11-24 17:11:19 -0800

    Add very basic sxhkd config, update installation scripts

--- pacman-install.sh
+++ pacman-install.sh
@@ -12,7 +12,8 @@ sudo pacman --needed -S fish xorg-server xfce4 neovim xsel base-devel vim git \
 	discord shellcheck gnome-keyring seahorse qalculate-gtk \
 	adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts \
 	network-manager-applet zathura zathura-pdf-poppler htop code fzf nnn \
-	ttf-ubuntu-font-family kitty kitty-terminfo highlight mediainfo mlocate
+	ttf-ubuntu-font-family kitty kitty-terminfo highlight mediainfo mlocate \
+	noto-fonts-emoji ttf-joypixels xdotool wmctrl
 
 # Install yay
 if [ ! -d "$HOME"/Downloads ]; then
