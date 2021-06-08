157ac41269c5c2266ca860bb31e4f06302310d26
Christopher Chang <chiggiechang@gmail.com>
2021-06-08 04:56:24 -0700

    Add transparency to wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,8 @@ return {
         {key="t", mods="ALT", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
         {key="h", mods="ALT", action=wezterm.action{ActivatePaneDirection="Left"}},
         {key="l", mods="ALT", action=wezterm.action{ActivatePaneDirection="Right"}},
+        {key="1", mods="ALT", action=wezterm.action{ActivateTab=1}},
+        {key="2", mods="ALT", action=wezterm.action{ActivateTab=2}},
         {key="H", mods="CTRL", action=wezterm.action{ActivateTabRelative=-1}},
         {key="L", mods="CTRL", action=wezterm.action{ActivateTabRelative=1}},
         {key="\"", mods="CTRL|ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
@@ -17,5 +19,6 @@ return {
     font_size = 12.0, -- This is the default font size anyway
     color_scheme = "Gruvbox Dark",
     hide_tab_bar_if_only_one_tab = true,
+    window_background_opacity = 0.95,
     warn_about_missing_glyphs = false
 }
