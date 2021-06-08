a09c8303cb3040d0fda3df7314cd7cea94896a26
Christopher Chang <chiggiechang@gmail.com>
2021-06-08 05:16:26 -0700

    Use gruvbox dark-esque colors for tab bar in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,10 +15,72 @@ return {
         {key="%", mods="CTRL|ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}}
     },
 
+    colors = {
+        -- Tab bar colors are roughly based off of the Gruvbox Dark colors found at:
+        -- https://github.com/mbadolato/iTerm2-Color-Schemes/blob/master/wezterm/Gruvbox%20Dark.toml
+        tab_bar = {
+
+            -- The color of the strip that goes along the top of the window
+            background = "#1e1e1e",
+
+            -- The active tab is the one that has focus in the window
+            active_tab = {
+                -- The color of the background area for the tab
+                bg_color = "#1e1e1e",
+                -- The color of the text for the tab
+                fg_color = "#aab01e",
+
+                -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+                -- label shown for this tab.
+                -- The default is "Normal"
+                intensity = "Bold",
+
+                -- Specify whether you want "None", "Single" or "Double" underline for
+                -- label shown for this tab.
+                -- The default is "None"
+                underline = "Single",
+
+                -- Specify whether you want the text to be italic (true) or not (false)
+                -- for this tab.  The default is false.
+                italic = false,
+
+                -- Specify whether you want the text to be rendered with strikethrough (true)
+                -- or not for this tab.  The default is false.
+                strikethrough = false,
+            },
+
+            -- Inactive tabs are the tabs that do not have focus
+            inactive_tab = {
+                bg_color = "#2f2f2f",
+                fg_color = "#999",
+
+                -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+                -- label shown for this tab.
+                -- The default is "Normal"
+                intensity = "Normal",
+
+                -- The same options that were listed under the `active_tab` section above
+                -- can also be used for `inactive_tab`.
+            },
+
+            -- You can configure some alternate styling when the mouse pointer
+            -- moves over inactive tabs
+            inactive_tab_hover = {
+                bg_color = "#484848",
+                fg_color = "#f7b125",
+                italic = true,
+
+                -- The same options that were listed under the `active_tab` section above
+                -- can also be used for `inactive_tab_hover`.
+            }
+        }
+    },
+
     font = wezterm.font("Inconsolata"),
     font_size = 12.0, -- This is the default font size anyway
     color_scheme = "Gruvbox Dark",
     hide_tab_bar_if_only_one_tab = true,
+    tab_bar_at_bottom = true,
     window_background_opacity = 0.95,
     warn_about_missing_glyphs = false
 }
