d491ccdd245fc1ad8a433ee8dc471fd62f68d5e2
Christopher Chang <chiggiechang@gmail.com>
2021-07-02 19:26:04 -0700

    Change mman shell function to run fzf in a loop

--- sourceable_scripts/mman.sh
+++ sourceable_scripts/mman.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
 mman() {
-  man $(man -k "$1" | fzf | awk '{print $1}')
+	for (( ; ; )) do
+		INPUT=$(man -k "$1" | fzf --reverse --height=15 | awk '{print $1}')
+		if [ "$INPUT" = "" ]; then
+			break
+		fi
+  	man "$INPUT"
+	done
 }
