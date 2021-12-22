86b9b6d3008685d6a13bc895b15a456281af0326
Christopher Chang <chiggiechang@gmail.com>
2021-12-22 05:07:16 -0800

    Kill cbatticon and redshift before restarting awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -401,7 +401,11 @@ keybindings["normal"] = gears.table.join(
               {description = "open a terminal", group = "launcher"}),
     -- awful.key({ modkey, "Control" }, "r", awesome.restart,
     --           {description = "reload awesome", group = "awesome"}),
-    awful.key({ modkey, "Shift" }, "r", awesome.restart,
+    awful.key({ modkey, "Shift" }, "r",
+        function ()
+            awful.spawn("killall cbatticon redshift") 
+            awesome.restart()
+        end,
               {description = "reload awesome", group = "awesome"}),
     -- awful.key({ modkey, "Shift"   }, "q", awesome.quit,
     --           {description = "quit awesome", group = "awesome"}),
