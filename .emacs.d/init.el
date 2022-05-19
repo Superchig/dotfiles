7ab53511aa6e7563ccfdec2d16f4eee84bf7880d
Christopher Chang <chiggiechang@gmail.com>
2022-05-18 20:21:10 -0700

    Remove unused ivy and slime package code from config for Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -73,29 +73,6 @@
 
 (use-package swiper)
 
-;; (use-package ivy
-;;   :config
-;;   (ivy-mode)
-;;   (setq ivy-use-virtual-buffers t)
-;;   (setq enable-recursive-minibuffers t)
-;;   (global-set-key "\C-s" 'swiper)
-;;   (global-set-key (kbd "C-c C-r") 'ivy-resume)
-;;   (global-set-key (kbd "<f6>") 'ivy-resume)
-;;   (global-set-key (kbd "M-x") 'counsel-M-x)
-;;   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-;;   (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-;;   (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-;;   (global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-;;   (global-set-key (kbd "<f1> l") 'counsel-find-library)
-;;   (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-;;   (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-;;   (global-set-key (kbd "C-c g") 'counsel-git)
-;;   (global-set-key (kbd "C-c j") 'counsel-git-grep)
-;;   (global-set-key (kbd "C-c k") 'counsel-ag)
-;;   (global-set-key (kbd "C-x l") 'counsel-locate)
-;;   (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-;;   (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
-
 (use-package vertico
   :init
   (vertico-mode)
@@ -152,21 +129,6 @@
 (defun call-prog-mode-hook ()
   (interactive)
   (dolist (hook prog-mode-hook) (funcall hook)))
-
-;; (use-package slime
-;;   :config
-;;   (setq inferior-lisp-program "sbcl")
-;;   ;; (add-hook 'slime-repl-mode-hook 'call-prog-mode-hook)
-;;   (add-hook 'slime-repl-mode-hook 'smartparens-strict-mode))
-
-;; (use-package slime-company
-;;   :after
-;;   (slime company)
-;;   :config
-;;   (setq slime-company-completion 'fuzzy
-;; 	slime-company-after-completion 'slime-company-just-one-space))
-
-;; (slime-setup '(slime-fancy slime-company))
 
 (use-package sly
   :config
