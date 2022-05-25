051c374f17b0c3a7a0d6904c8de119813c147b70
Christopher Chang <chiggiechang@gmail.com>
2022-05-24 23:49:21 -0700

    Add window-split-toggle function to Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -64,6 +64,21 @@
                   (get-char-property (point) 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
 
+;; https://emacs.stackexchange.com/questions/5371/how-to-change-emacs-windows-from-vertical-split-to-horizontal-split
+(defun window-split-toggle ()
+  "Toggle between horizontal and vertical split with two windows."
+  (interactive)
+  (if (> (length (window-list)) 2)
+      (error "Can't toggle with more than 2 windows!")
+    (let ((func (if (window-full-height-p)
+                    #'split-window-vertically
+                  #'split-window-horizontally)))
+      (delete-other-windows)
+      (funcall func)
+      (save-selected-window
+        (other-window 1)
+        (switch-to-buffer (other-buffer))))))
+
 (defun ee ()
   "Edit the Emacs configuration file."
   (interactive)
