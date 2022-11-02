20be2edeab4d059f887c0bda22dccaf17fae422d
Christopher Chang <chiggiechang@gmail.com>
2022-11-02 10:51:16 -0700

    Use default_capabilities rather than make_capabilities in Neovim config
    https://www.reddit.com/r/neovim/comments/y97pl3/cmp_nvim_lspupdate_capabilities_is_deprecated_use/

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -267,7 +267,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Prepare for lspconfig
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require('nvim-autopairs').setup()
 
