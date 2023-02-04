4914d054ff0007a037813dbc6e35f92ecad6257b
Christopher Chang <chiggiechang@gmail.com>
2023-02-04 13:18:01 -0800

    Set colorscheme dynamically in Neovim (based on OS/front-end)

--- stow/nvim/.config/nvim/lua/plugins/themes.lua
+++ stow/nvim/.config/nvim/lua/plugins/themes.lua
@@ -12,7 +12,29 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      -- NOTE(Chris): This anonymous function returns the name of the
+      -- colorscheme we want to use, and it may set the font or other
+      -- UI settings.
+      colorscheme = (function()
+        if vim.g.neovide then
+          -- vim.g.neovide_transparency = 0.8
+
+          if vim.loop.os_uname().sysname == "Linux" then
+            vim.o.guifont = "Iosevka Nerd Font:h12,Consolas:h12,Inconsolata:h12"
+          else
+            vim.o.guifont = "Iosevka Term:h12,Consolas:h12,Inconsolata:h12"
+          end
+
+          return "tokyonight"
+        end
+
+        if vim.loop.os_uname().sysname == "Darwin" then
+          vim.o.background = "light"
+          return "one-nvim"
+        else
+          return "gruvbox"
+        end
+      end)(),
     },
   },
 }
