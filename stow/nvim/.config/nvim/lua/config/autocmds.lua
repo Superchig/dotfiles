ced2e86d11e6572ec2f06351721b65bf73449e40
Christopher Chang <chiggiechang@gmail.com>
2023-02-04 12:31:53 -0800

    Disable completion in Markdown buffers in Neovim

--- stow/nvim/.config/nvim/lua/config/autocmds.lua
+++ stow/nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- TODO(Chris): Put this in the config part of some plugin configuration
+vim.cmd([[autocmd Filetype markdown lua require('cmp').setup.buffer { enabled = false }]])
