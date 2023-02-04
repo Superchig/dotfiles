e1f88f301baf4fc8d04fbb4c3e9ff62a4b77ef62
Christopher Chang <chiggiechang@gmail.com>
2023-02-04 03:54:15 -0800

    Remove concealer and dirman modules from Neorg config

--- stow/nvim/.config/nvim/lua/plugins/neorg.lua
+++ stow/nvim/.config/nvim/lua/plugins/neorg.lua
@@ -7,14 +7,6 @@ return {
   opts = {
     load = {
       ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-      ["core.norg.dirman"] = { -- Manages Neorg workspaces
-        config = {
-          workspaces = {
-            notes = "~/notes",
-          },
-        },
-      },
     },
   },
   dependencies = { { "nvim-lua/plenary.nvim" } },
