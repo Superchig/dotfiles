f42e2c267019131227c3806bfb46d183e832275f
Christopher Chang <chiggiechang@gmail.com>
2023-02-04 13:18:01 -0800

    Set colorscheme dynamically in Neovim (based on OS/front-end)

--- stow/nvim/.config/nvim/lua/config/options.lua
+++ stow/nvim/.config/nvim/lua/config/options.lua
@@ -7,13 +7,3 @@ vim.o.timeoutlen = 0
 vim.opt.completeopt = "menuone,noinsert,noselect"
 vim.opt.hlsearch = false
 vim.opt.conceallevel = 0
-
-if vim.g.neovide then
-  -- vim.g.neovide_transparency = 0.8
-
-  if vim.loop.os_uname().sysname == "Linux" then
-    vim.o.guifont = "Iosevka Nerd Font:h12,Consolas:h12,Inconsolata:h12"
-  else
-    vim.o.guifont = "Iosevka Term:h12,Consolas:h12,Inconsolata:h12"
-  end
-end
