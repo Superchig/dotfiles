6e9c644234623a1bfe2074135cc16c280032fc19
Christopher Chang <chiggiechang@gmail.com>
2023-03-12 00:27:39 -0800

    Set shiftwidth and tabstop for C++ files in Neovim

--- stow/nvim/.config/nvim/lua/config/autocmds.lua
+++ stow/nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,6 +7,8 @@ vim.cmd([[autocmd Filetype markdown lua require('cmp').setup.buffer { enabled = 
 
 vim.cmd([[autocmd Filetype markdown set textwidth=78 colorcolumn=+0]])
 
+vim.cmd([[autocmd Filetype cpp set shiftwidth=4 tabstop=4]])
+
 -- NOTE(Chris): Not sure where else to put this
 if vim.loop.os_uname().sysname == 'Darwin'
 then
