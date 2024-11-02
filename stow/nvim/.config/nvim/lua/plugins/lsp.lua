27620b4dd250e1ad344b7b33f8ed4600e15d9190
Christopher Chang <chiggiechang@gmail.com>
2024-11-02 01:56:46 -0700

    Use custom clangd CLI argument in Neovim

--- stow/nvim/.config/nvim/lua/plugins/lsp.lua
+++ stow/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,127 @@
 return {
+  -- Modified from https://www.lazyvim.org/plugins/lsp#nvim-lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      ---@class PluginLspOpts
+      local ret = {
+        -- options for vim.diagnostic.config()
+        ---@type vim.diagnostic.Opts
+        diagnostics = {
+          underline = true,
+          update_in_insert = false,
+          virtual_text = {
+            spacing = 4,
+            source = "if_many",
+            prefix = "●",
+            -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+            -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+            -- prefix = "icons",
+          },
+          severity_sort = true,
+          signs = {
+            text = {
+              [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+              [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+              [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+              [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+            },
+          },
+        },
+        -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the inlay hints.
+        inlay_hints = {
+          enabled = true,
+          exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+        },
+        -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the code lenses.
+        codelens = {
+          enabled = false,
+        },
+        -- Enable lsp cursor word highlighting
+        document_highlight = {
+          enabled = true,
+        },
+        -- add any global capabilities here
+        capabilities = {
+          workspace = {
+            fileOperations = {
+              didRename = true,
+              willRename = true,
+            },
+          },
+        },
+        -- options for vim.lsp.buf.format
+        -- `bufnr` and `filter` is handled by the LazyVim formatter,
+        -- but can be also overridden when specified
+        format = {
+          formatting_options = nil,
+          timeout_ms = nil,
+        },
+        -- LSP Server Settings
+        servers = {
+          lua_ls = {
+            -- mason = false, -- set to false if you don't want this server to be installed with mason
+            -- Use this to add any additional keymaps
+            -- for specific lsp servers
+            -- ---@type LazyKeysSpec[]
+            -- keys = {},
+            settings = {
+              Lua = {
+                workspace = {
+                  checkThirdParty = false,
+                },
+                codeLens = {
+                  enable = true,
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+                doc = {
+                  privateName = { "^_" },
+                },
+                hint = {
+                  enable = true,
+                  setType = false,
+                  paramType = true,
+                  paramName = "Disable",
+                  semicolon = "Disable",
+                  arrayIndex = "Disable",
+                },
+              },
+            },
+          },
+        },
+        -- you can do any additional lsp server setup here
+        -- return true if you don't want this server to be setup with lspconfig
+        setup = {
+          -- example to setup with typescript.nvim
+          -- tsserver = function(_, opts)
+          --   require("typescript").setup({ server = opts })
+          --   return true
+          -- end,
+          -- Specify * to use this function as a fallback for any server
+          -- ["*"] = function(server, opts) end,
+          clangd = function(_, opts)
+            require("lspconfig").clangd.setup({
+              server = opts,
+              -- Parmeters are (new_config, root_dir)
+              on_new_config = function(new_config, _)
+                -- From https://clangd.llvm.org/features#experimental-c20-modules-support
+                table.insert(new_config.cmd, "--experimental-modules-support")
+              end,
+            })
+            return true
+          end,
+        },
+      }
+      return ret
+    end,
+  },
+
   {
     "hrsh7th/nvim-cmp",
     -- Modified from https://www.lazyvim.org/plugins/coding#nvim-cmp
@@ -47,20 +170,20 @@ return {
           },
         },
       }
-    end
+    end,
   },
 
   {
     "L3MON4D3/LuaSnip",
-    opts = function ()
+    opts = function()
       return {
         -- NOTE(Chris): This stops you from jumping to a snippet with <tab> once you've exited the snippet
         history = false,
       }
-    end
+    end,
     -- NOTE(Chris): Uncomment this if you just want to disable the <tab> jump key
     -- keys = function ()
     --   return { }
     -- end
-  }
+  },
 }
