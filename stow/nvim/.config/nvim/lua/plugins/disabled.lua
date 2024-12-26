c2a2bb4b7608dde2951220226e5af00dffa6c80f
Christopher Chang <chiggiechang@gmail.com>
2024-12-26 15:10:55 -0800

    Reenable some default LazyVim functionality

--- stow/nvim/.config/nvim/lua/plugins/disabled.lua
+++ stow/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -3,15 +3,15 @@ return {
   { "ggandor/leap.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
-  -- I haven't takent the time to rebind this plugin to correspond to the
+  -- I haven't taken the time to rebind this plugin to correspond to the
   -- vim-commentary mappings
-  { "echasnovski/mini.surround", enabled = false },
+  -- { "echasnovski/mini.surround", enabled = false },
   -- I've never gotten used to
-  { "akinsho/bufferline.nvim", enabled = false },
+  -- { "akinsho/bufferline.nvim", enabled = false },
   -- As of 2/4/2023, this doesn't work well with Neovide
   { "folke/noice.nvim", enabled = false },
   -- As of 2/4/2023, this doesn't support highlighting the author in TODO(author):
   -- https://github.com/folke/todo-comments.nvim/issues/10
   { "folke/todo-comments.nvim", enabled = false },
-  { "RRethy/vim-illuminate", enabled = false },
+  -- { "RRethy/vim-illuminate", enabled = false },
 }
