1ea2507865493c059bb805fbf0e68b2eeb661abc
Christopher Chang <chiggiechang@gmail.com>
2024-12-26 15:39:29 -0800

    Configure snacks.nvim to have less animation

--- stow/nvim/.config/nvim/lua/plugins/override.lua
+++ stow/nvim/.config/nvim/lua/plugins/override.lua
@@ -56,4 +56,17 @@ return {
       },
     },
   },
+
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+      indent = {
+        enabled = true,
+        animate = {
+          enabled = false,
+        },
+      },
+    },
+  },
 }
