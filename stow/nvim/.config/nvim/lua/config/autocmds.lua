658b3d85e95478a665d1f6ab0f8ed4d54faa3478
Christopher Chang <chiggiechang@gmail.com>
2024-12-26 15:33:40 -0800

    Remove completion-disabling line

--- stow/nvim/.config/nvim/lua/config/autocmds.lua
+++ stow/nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,9 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- TODO(Chris): Put this in the config part of some plugin configuration
-vim.cmd([[autocmd Filetype markdown lua require('cmp').setup.buffer { enabled = false }]])
-
 vim.cmd([[autocmd Filetype markdown set textwidth=78 colorcolumn=+0]])
 
 vim.cmd([[autocmd Filetype c,cpp set shiftwidth=4 tabstop=4]])
