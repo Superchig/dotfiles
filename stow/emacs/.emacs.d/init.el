a08e91040a7096e3b00f5155de3a5759e5dab2a4
Christopher Chang <chiggiechang@gmail.com>
2025-03-02 01:24:30 -0800

    Switch to straight.el in Emacs

--- stow/emacs/.emacs.d/init.el
+++ stow/emacs/.emacs.d/init.el
@@ -44,11 +44,26 @@
   (hl-line-mode -1))
 (add-hook 'term-mode-hook 'my/term-mode-hook)
 
-;; You can update the GNU ELPA keyring with `package-install gnu-elpa-keyring-update'
-;; https://lists.gnu.org/archive/html/emacs-devel/2024-06/msg01157.html
+(defun install-straight-el ()
+  (interactive)
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name
+          "straight/repos/straight.el/bootstrap.el"
+          (or (bound-and-true-p straight-base-dir)
+              user-emacs-directory)))
+	(bootstrap-version 7))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage)))
 
-(package-initialize)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(install-straight-el)
+(setq straight-use-package-by-default t)
 
 (use-package doom-themes
   :ensure t
@@ -173,17 +188,3 @@
 ;; (add-hook 'prog-mode-hook
 ;; 	  (lambda ()
 ;; 	    (setq require-final-newline t)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(geiser-guile geiser git-gutter magit keycast paredit ace-window gnu-elpa-keyring-update vertico company sly doom-themes which-key)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
