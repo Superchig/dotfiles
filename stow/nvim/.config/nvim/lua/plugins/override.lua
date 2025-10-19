f60d26e4d90aecb6c23ab878849bd211fa5c1e79
Christopher Chang <chiggiechang@gmail.com>
2025-10-19 01:14:34 -0700

    Use csharpier-client with C#

--- stow/nvim/.config/nvim/lua/plugins/override.lua
+++ stow/nvim/.config/nvim/lua/plugins/override.lua
@@ -243,4 +243,43 @@ return {
       },
     },
   },
+
+  {
+    "stevearc/conform.nvim",
+    opts = function()
+      local opts = {
+        default_format_opts = {
+          timeout_ms = 3000,
+          async = false, -- not recommended to change
+          quiet = false, -- not recommended to change
+          lsp_format = "fallback", -- not recommended to change
+        },
+        formatters_by_ft = {
+          lua = { "stylua" },
+          fish = { "fish_indent" },
+          sh = { "shfmt" },
+          cs = { "csharpier-client" },
+        },
+        formatters = {
+          injected = { options = { ignore_errors = true } },
+          ["csharpier-client"] = {
+            command = "csharpier-client",
+            args = { "$FILENAME" },
+          },
+          -- # Example of using dprint only when a dprint.json file is present
+          -- dprint = {
+          --   condition = function(ctx)
+          --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+          --   end,
+          -- },
+          --
+          -- # Example of using shfmt with extra args
+          -- shfmt = {
+          --   prepend_args = { "-i", "2", "-ci" },
+          -- },
+        },
+      }
+      return opts
+    end,
+  },
 }
